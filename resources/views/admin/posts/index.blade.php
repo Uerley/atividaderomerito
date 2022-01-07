cbf834723517c982df6021b4c27dfda5852c8ec1
Uerley Gabriel <71016901+Uerley@users.noreply.github.com>
2022-01-07 00:50:26 -0300

    TitleFilme

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,4 +1,13 @@
-<a href="{{route('posts.create')}}">Criar Novo Post</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Filme</title>
+</head>
+<body>
+    <a href="{{route('posts.create')}}">Criar Novo Post</a>
 <hr>
 @if(session('message'))
     <div>
@@ -13,6 +22,7 @@
         <a href="{{ route('posts.show', $post->id) }}">Ver</a> |
      ]</p>
 @endforeach
-
-
 <hr>
+</body>
+</html>
+
